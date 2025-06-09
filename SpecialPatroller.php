5add73293e2c0e1c86ad1bf87e98bfe1230b793a
eckserah <eckserah@gmail.com>
2025-06-08 20:20:09 -0400

    Backport bugfix from 1.39

--- SpecialPatroller.php
+++ SpecialPatroller.php
@@ -218,7 +218,7 @@ class SpecialPatroller extends SpecialPage {
 		$uid = $user->getId();
 		extract( $dbr->tableNames( 'recentchanges', 'patrollers', 'page' ) );
 		$res = $dbr->select(
-			[ $page, $recentchanges ],
+			[ $page, $recentchanges, $patrollers ],
 			'*',
 			[
 				'ptr_timestamp IS NULL',
@@ -232,7 +232,7 @@ class SpecialPatroller extends SpecialPage {
 			],
 			__METHOD__,
 			[
-				'LIMIT'	=> '0,1'
+				'LIMIT'	=> 1
 			],
 			[
 				$patrollers => [
